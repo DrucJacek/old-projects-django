85158770be418b23cdb05452961e0a3e6873a687
Jacek Druć <jacek.druc@infotech.edu.pl>
2024-09-10 09:17:08 +0200

    created zip

--- djangoGrzyby/djangogrzyby/grzybyapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Grzyby, Rodzina, Potrawy
-
-admin.site.register(Grzyby)
-admin.site.register(Rodzina)
-admin.site.register(Potrawy)
--- djangoGrzyby/djangogrzyby/grzybyapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Grzyby
-
-def grzyby(request):
-    grzyby = Grzyby.objects.all()
-    return render(request, "grzybyapp/index.html", {"grzyby": grzyby})
